e89ae1b71c3e9d14aff14d6f6de4d0437ad6dbfa
Tên của bạn <jungjihoonkr97@gmail.com>
2022-09-03 12:52:59 +0700

    code homepage blog basic

--- src/components/header/UIHeader.tsx
+++ src/components/header/UIHeader.tsx
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 
+import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import SearchIcon from '@mui/icons-material/Search';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+import Button from '@mui/material/Button';
 
 import logo from '../../assets/images/logo.svg';
 
 import '../header/style.scss';
 
 interface HeaderProps {
-  sections: ReadonlyArray<{
+  sections: Array<{
     title: string;
     url: string;
+    description: string;
   }>;
   title: string;
 }
@@ -24,52 +25,52 @@ export class UIHeader extends Component<HeaderProps> {
 
     return (
       <React.Fragment>
-        <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
-          <Typography
-            component="h1"
-            variant="h5"
-            color="inherit"
-            align="left"
-            noWrap
-            mt={1}
-            sx={{ flex: 1 }}
-          >
-            {title}
-          </Typography>
-          <Typography
-            component="h1"
-            variant="h5"
-            color="inherit"
-            align="left"
-            noWrap
-            mt={1}
-            sx={{ flex: 1 }}
-          >
-            <img src={logo} className="App-logo-header" alt="logo" />
-          </Typography>
-          <IconButton>
-            <SearchIcon />
-          </IconButton>
-        </Toolbar>
-        <Toolbar
-          component="nav"
-          variant="dense"
-          sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
-        >
-          {sections.map(section => (
-            <Link
-              color="primary"
-              rel="noopener"
-              underline="none"
-              key={section.title}
-              variant="body2"
-              href={section.url}
-              sx={{ p: 2, flexShrink: 0 }}
+        <AppBar position="relative">
+          <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
+            <Typography
+              component="h1"
+              variant="h5"
+              color="inherit"
+              align="left"
+              noWrap
+              mt={1}
+              sx={{ flex: 1 }}
             >
-              {section.title}
-            </Link>
-          ))}
-        </Toolbar>
+              {title}
+            </Typography>
+            <Typography
+              component="h1"
+              variant="h5"
+              color="inherit"
+              align="left"
+              noWrap
+              mt={1}
+              sx={{ flex: 1 }}
+            >
+              <img src={logo} className="App-logo-header" alt="logo" />
+            </Typography>
+            <Button variant="outlined" size="small"></Button>
+          </Toolbar>
+          <Toolbar
+            component="nav"
+            variant="dense"
+            sx={{ justifyContent: 'center', overflowX: 'auto' }}
+          >
+            {sections.map(section => (
+              <Link
+                color="inherit"
+                rel="noopener"
+                underline="none"
+                key={section.title}
+                variant="body1"
+                href={section.url}
+                sx={{ p: 2, flexShrink: 0 }}
+              >
+                {section.title}
+              </Link>
+            ))}
+          </Toolbar>
+        </AppBar>
       </React.Fragment>
     );
   }
